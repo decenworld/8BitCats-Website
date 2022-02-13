c66ef7e409d3229a790f284d9ba8d5976193f34b
steffanjensen <6hourapp@gmail.com>
2022-02-14 00:26:46 +0100

    website deployed

---
+++ src/redux/data/dataActions.js
@@ -0,0 +1,54 @@
+// log
+import store from "../store";
+
+const fetchDataRequest = () => {
+  return {
+    type: "CHECK_DATA_REQUEST",
+  };
+};
+
+const fetchDataSuccess = (payload) => {
+  return {
+    type: "CHECK_DATA_SUCCESS",
+    payload: payload,
+  };
+};
+
+const fetchDataFailed = (payload) => {
+  return {
+    type: "CHECK_DATA_FAILED",
+    payload: payload,
+  };
+};
+
+export const fetchData = (account) => {
+  return async (dispatch) => {
+    dispatch(fetchDataRequest());
+    try {
+      let totalSupply = await store
+        .getState()
+        .blockchain.smartContract.methods.totalSupply()
+        .call();
+        let allTokens = await store
+        .getState()
+        .blockchain.smartContract.methods.walletOfOwner(account)
+        .call();
+      // let cost = await store
+      //   .getState()
+      //   .blockchain.smartContract.methods.cost()
+      //   .call();
+
+      dispatch(
+        fetchDataSuccess({
+          totalSupply,
+          name,
+          allTokens,
+          // cost,
+        })
+      );
+    } catch (err) {
+      console.log(err);
+      dispatch(fetchDataFailed("Could not load data from contract."));
+    }
+  };
+};
---
+++ src/redux/data/dataReducer.js
@@ -0,0 +1,43 @@
+const initialState = {
+  loading: false,
+  name: "",
+  allTokens: [],
+  totalSupply: 0,
+  cost: 0,
+  error: false,
+  errorMsg: "",
+};
+
+const dataReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case "CHECK_DATA_REQUEST":
+      return {
+        ...state,
+        loading: true,
+        error: false,
+        errorMsg: "",
+      };
+    case "CHECK_DATA_SUCCESS":
+      return {
+        ...state,
+        loading: false,
+        name: action.payload.name,
+        allTokens: action.payload.allTokens,
+        totalSupply: action.payload.totalSupply,
+        // cost: action.payload.cost,
+        error: false,
+        errorMsg: "",
+      };
+    case "CHECK_DATA_FAILED":
+      return {
+        ...initialState,
+        loading: false,
+        error: true,
+        errorMsg: action.payload,
+      };
+    default:
+      return state;
+  }
+};
+
+export default dataReducer;
