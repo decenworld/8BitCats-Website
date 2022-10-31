e083b333a9427e236c84e5939ac9df195e20d656
Steffan Jensen <6hourapp@gmail.com>
2022-10-31 06:51:12 +0100

    0.21
    - Usage for Catcoin
    - New animal 3D CAT
    - MINT NFTs for Catcoin

--- src/views/3dcat/Catland.js
+++ src/views/3dcat/Catland.js
@@ -224,7 +224,7 @@ const [mintAmount, setMintAmount] = useState(1);
                      <img style={{width: '200px', height:'200px', border: '0px black solid'}} src={require('./example.gif')} />
           <Grid container justify="center" spacing={0} style={{marginTop: '10px', marginBottom: '10px'}}>
 
-                      <h4 style={{ textAlign:'center', marginBottom: '2px'  }}>1 3D CAT <p><br></br></p>MINT PRICE: 10 CATCOIN</h4>
+                      <h4 style={{ textAlign:'center', marginBottom: '2px'  }}>1 3D CAT <p><br></br></p>MINT PRICE: 10 CATCOINS</h4>
                   </Grid>
 
               <span>
