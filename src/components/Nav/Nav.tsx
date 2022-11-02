8fa55b61556d8bd3533fca860ce9364a1f5f513f
TBA187 <sadri_albaner@hotmail.com>
2022-11-02 23:07:11 +0100

    Link to FTM Llama added on mobile Nav +code format

--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -90,77 +90,66 @@ const Nav = () => {
     setOpen(false);
   };
 
-
-
   return (
     <AppBar position="sticky" elevation={0} className={classes.appBar}>
       <Toolbar className={classes.toolbar}>
         {matches ? (
           <>
-            <Typography variant="h6" color="inherit" noWrap className={classes.toolbarTitle}>     
-            <a rel="noopener noreferrer" href="https://8bitcats.com" className={classes.link}>
-              <img alt="MiniVerse" src={mvLogo} height="50px" style={{marginTop: '15px'}} />
-              </a> 
+            <Typography variant="h6" color="inherit" noWrap className={classes.toolbarTitle}>
+              <a rel="noopener noreferrer" href="https://8bitcats.com" className={classes.link}>
+                <img alt="MiniVerse" src={mvLogo} height="50px" style={{ marginTop: '15px' }} />
+              </a>
             </Typography>
-             <AccountButton text="Connect" />
+            <AccountButton text="Connect" />
             <Box mr={5}>
               <Link color="textPrimary" to="/" className={classes.link}>
                 Home
               </Link>
-                                <Link color="textPrimary" to="/boardroom" className={classes.link}>
+              <Link color="textPrimary" to="/boardroom" className={classes.link}>
                 Boardroom
               </Link>
-                                <Link color="textPrimary" to="/farms" className={classes.link}>
+              <Link color="textPrimary" to="/farms" className={classes.link}>
                 Farms
               </Link>
-                                            <Link color="textPrimary" to="/catstatue" className={classes.link}>
+              <Link color="textPrimary" to="/catstatue" className={classes.link}>
                 Golden Statue
               </Link>
-                                    <Link color="textPrimary" to="/diamondstone" className={classes.link}>
+              <Link color="textPrimary" to="/diamondstone" className={classes.link}>
                 Diamond Stone
               </Link>
-                                                  <Link color="textPrimary" to="/3dcat" className={classes.link}>
+              <Link color="textPrimary" to="/3dcat" className={classes.link}>
                 3D Cats
               </Link>
-                                    <Link color="textPrimary" to="/seal" className={classes.link}>
+              <Link color="textPrimary" to="/seal" className={classes.link}>
                 Seals
               </Link>
-                      
-                      <Link color="textPrimary" to="/penguin" className={classes.link}>
+              <Link color="textPrimary" to="/penguin" className={classes.link}>
                 Penguins
               </Link>
-                    <Link color="textPrimary" to="/owl" className={classes.link}>
+              <Link color="textPrimary" to="/owl" className={classes.link}>
                 Owls
               </Link>
-                                  <Link color="textPrimary" to="/trippycat" className={classes.link}>
+              <Link color="textPrimary" to="/trippycat" className={classes.link}>
                 Trippycat
               </Link>
-
-                            <Link color="textPrimary" to="/cats" className={classes.link}>
+              <Link color="textPrimary" to="/cats" className={classes.link}>
                 8-Bit Cats
               </Link>
-
               <Link color="textPrimary" to="/cryptoman" className={classes.link}>
                 Cryptoman
               </Link>
-               
-                <Link color="textPrimary" to="/fantomllama" className={classes.link}>
+              <Link color="textPrimary" to="/fantomllama" className={classes.link}>
                 Fantom Llama
               </Link>
-                          <Link color="textPrimary" to="/catland" className={classes.link}>
+              <Link color="textPrimary" to="/catland" className={classes.link}>
                 CatLand
               </Link>
-           
-            
-                      <Link color="textPrimary" to="/info" className={classes.link}>
+              <Link color="textPrimary" to="/info" className={classes.link}>
                 Info
               </Link>
-                                    <Link color="textPrimary" to={{ pathname:`https://8bitcats.gitbook.io/whitepaper/`}} target="_blank" className={classes.link}>
+              <Link color="textPrimary" to={{ pathname: `https://8bitcats.gitbook.io/whitepaper/` }} target="_blank" className={classes.link}>
                 Whitepaper
               </Link>
-
-
-
 
               {/* <Link color="textPrimary" to="/dividends" className={classes.link}>
                 Dividends
@@ -174,14 +163,11 @@ const Nav = () => {
               <Link color="textPrimary" to="/regulations" className={classes.link}>
                 Regulations
               </Link> */}
-             
               {/* <a target="_blank" href="https://docs.2omb.finance/contracts/tokens" className={classes.link}>
                 Contracts
               </a> */}
-        
 
             </Box>
-           
           </>
         ) : (
           <>
@@ -195,8 +181,8 @@ const Nav = () => {
               <MenuIcon />
             </IconButton>
             <Typography variant="h6" noWrap>
-            <a rel="noopener noreferrer" href="https://8bitcats.com/" className={classes.link}>
-              8-Bit Cats
+              <a rel="noopener noreferrer" href="https://8bitcats.com/" className={classes.link}>
+                8-Bit Cats
               </a>
             </Typography>
 
@@ -218,7 +204,6 @@ const Nav = () => {
               </div>
               <Divider />
               <List>
-
                 <ListItem style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
                   <AccountButton text="Connect" />
                 </ListItem>
@@ -227,26 +212,20 @@ const Nav = () => {
                 <ListItemLink primary="FARMS" to="/farms" />
                 <ListItemLink primary="DIAMOND STONE" to="/diamondstone" />
                 <ListItemLink primary="CAT STATUE" to="/catstatue" />
-                                <ListItemLink primary="3D CAT" to="/3dcat" />
+                <ListItemLink primary="3D CAT" to="/3dcat" />
                 <ListItemLink primary="SEALS" to="/seal" />
                 <ListItemLink primary="TRIPPY CAT" to="/trippycat" />
                 <ListItemLink primary="PENGUINS" to="/penguin" />
                 <ListItemLink primary="OWLS" to="/owl" />
                 <ListItemLink primary="8-BIT CATS" to="/cats" />
                 <ListItemLink primary="CRYPTOMAN" to="/cryptoman" />
+                <ListItemLink primary="FANTOM LLAMA" to="/fantomllama" />
                 <ListItemLink primary="CAT LAND" to="/catland" />
-            
                 <ListItemLink primary="INFO" to="/info" />
-                       <ListItem button component="a" href="https://8bitcats.gitbook.io/whitepaper/">
-    <ListItemText primary="WHITEPAPER" />
-  </ListItem>     
-
-
-
-
-                
+                <ListItem button component="a" href="https://8bitcats.gitbook.io/whitepaper/">
+                  <ListItemText primary="WHITEPAPER" />
+                </ListItem>
               </List>
-
             </Drawer>
           </>
         )}
